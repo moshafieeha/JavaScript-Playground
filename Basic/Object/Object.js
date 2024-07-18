5b29ecab6c450b2a3b0b7a8c4e04dd6a0dfca8f5
moshafieeha <m.shafieeha@gmail.com>
2024-07-18 15:28:15 +0330

    Update Object.js

--- Basic/Object/Object.js
+++ Basic/Object/Object.js
@@ -1,86 +1,86 @@
 const fs = require("fs");
-// const emailData = fs.readFileSync("emails.json", "utf8");
-// const emails = JSON.parse(emailData);
+const emailData = fs.readFileSync("emails.json", "utf8");
+const emails = JSON.parse(emailData);
 
-// // Function to count Yahoo and Gmail emails
-// function countEmails(emails) {
-//   let yahooCount = 0;
-//   let gmailCount = 0;
+// Function to count Yahoo and Gmail emails
+function countEmails(emails) {
+  let yahooCount = 0;
+  let gmailCount = 0;
 
-//   for (let email of emails) {
-//     if (email.includes("@yahoo.com")) {
-//       yahooCount++;
-//     } else if (email.includes("@gmail.com")) {
-//       gmailCount++;
-//     }
-//   }
+  for (let email of emails) {
+    if (email.includes("@yahoo.com")) {
+      yahooCount++;
+    } else if (email.includes("@gmail.com")) {
+      gmailCount++;
+    }
+  }
 
-//   console.log(yahooCount);
-//   console.log(gmailCount);
-// }
+  console.log(yahooCount);
+  console.log(gmailCount);
+}
 
-// countEmails(emails);
-// //////////////////////////////////////////////////////////////////////////////
+countEmails(emails);
+//////////////////////////////////////////////////////////////////////////////
 
-// // Function to show longest emails
-// function longestEmail(emails) {
-//   let maxLength = 0;
-//   let longestEmails = [];
+// Function to show longest emails
+function longestEmail(emails) {
+  let maxLength = 0;
+  let longestEmails = [];
 
-//   for (let email of emails) {
-//     if (email.length > maxLength) {
-//       maxLength = email.length;
-//       longestEmails = [email];
-//     } else if (email.length === maxLength) {
-//       longestEmails.push(email);
-//     }
-//   }
-//   console.log(longestEmails.join(", "));
-// }
+  for (let email of emails) {
+    if (email.length > maxLength) {
+      maxLength = email.length;
+      longestEmails = [email];
+    } else if (email.length === maxLength) {
+      longestEmails.push(email);
+    }
+  }
+  console.log(longestEmails.join(", "));
+}
 
-// longestEmail(emails);
-// //////////////////////////////////////////////////////////////////////////////
+longestEmail(emails);
+//////////////////////////////////////////////////////////////////////////////
 
-// // Function to show shortest emails
-// function shortestEmail(emails) {
-//   let minLength = Infinity;
-//   let shortestEmails = [];
+// Function to show shortest emails
+function shortestEmail(emails) {
+  let minLength = Infinity;
+  let shortestEmails = [];
 
-//   for (let email of emails) {
-//     if (email.length < minLength) {
-//       minLength = email.length;
-//       shortestEmails = [email];
-//     } else if (email.length === minLength) {
-//       shortestEmails.push(email);
-//     }
-//   }
-//   console.log(shortestEmails.join(", "));
-// }
+  for (let email of emails) {
+    if (email.length < minLength) {
+      minLength = email.length;
+      shortestEmails = [email];
+    } else if (email.length === minLength) {
+      shortestEmails.push(email);
+    }
+  }
+  console.log(shortestEmails.join(", "));
+}
 
-// shortestEmail(emails);
-// //////////////////////////////////////////////////////////////////////////////
+shortestEmail(emails);
+//////////////////////////////////////////////////////////////////////////////
 
-// // Function to filter Gmail without numbers
-// function gmailWithoutNu(emails) {
-//   let gmailWithoutNumbers = [];
+// Function to filter Gmail without numbers
+function gmailWithoutNu(emails) {
+  let gmailWithoutNumbers = [];
 
-//   for (let email of emails) {
-//     if (email.includes("@gmail.com")) {
-//       let hasNumber = false;
-//       for (let char of email) {
-//         if (char >= "0" && char <= "9") {
-//           hasNumber = true;
-//           break;
-//         }
-//       }
-//       if (!hasNumber) {
-//         gmailWithoutNumbers.push(email);
-//       }
-//     }
-//   }
-//   console.log(gmailWithoutNumbers.join(", "));
-// }
-// gmailWithoutNu(emails);
+  for (let email of emails) {
+    if (email.includes("@gmail.com")) {
+      let hasNumber = false;
+      for (let char of email) {
+        if (char >= "0" && char <= "9") {
+          hasNumber = true;
+          break;
+        }
+      }
+      if (!hasNumber) {
+        gmailWithoutNumbers.push(email);
+      }
+    }
+  }
+  console.log(gmailWithoutNumbers.join(", "));
+}
+gmailWithoutNu(emails);
 //////////////////////////////////////////////////////////////////////////////
 
 const userData = fs.readFileSync("users.json", "utf8");
@@ -178,12 +178,151 @@ function balanceAbove18(users) {
 console.log(balanceAbove18(users));
 //////////////////////////////////////////////////////////////////////////////
 
+// A functio to get the unique countries
+function uniqueCountries(users) {
+  const countries = [];
+  for (let i = 0; i < users.length; i++) {
+    if (!countries.includes(users[i].country)) {
+      countries.push(users[i].country);
+    }
+  }
+  return countries;
+}
+console.log(uniqueCountries(users));
 //////////////////////////////////////////////////////////////////////////////
 
+// A function to get full names
+function fullNames(users) {
+  const fullNames = [];
+  for (let i = 0; i < users.length; i++) {
+    fullNames.push(`${users[i].firstName} ${users[i].lastName}`);
+  }
+  return fullNames;
+}
+
+console.log(fullNames(users));
+////////////////////////////////////////////////////////////////////////////
+
+// Function to get user count by country
+function userCountByCountry(users) {
+  const countryCounts = {};
+  for (let i = 0; i < users.length; i++) {
+    const country = users[i].country;
+    if (countryCounts[country]) {
+      countryCounts[country]++;
+    } else {
+      countryCounts[country] = 1;
+    }
+  }
+  return countryCounts;
+}
+
+console.log(userCountByCountry(users));
+////////////////////////////////////////////////////////////////////////////
+
+// Function to get ave balance by country
+function aveBalanceByCountry(users) {
+  const countrySums = {};
+  const countryCounts = {};
+  const countryAverages = {};
+
+  for (let i = 0; i < users.length; i++) {
+    const country = users[i].country;
+    const balance = users[i].balance;
+    if (countrySums[country]) {
+      countrySums[country] += balance;
+      countryCounts[country]++;
+    } else {
+      countrySums[country] = balance;
+      countryCounts[country] = 1;
+    }
+  }
+
+  for (const country in countrySums) {
+    countryAverages[country] = countrySums[country] / countryCounts[country];
+  }
+
+  return countryAverages;
+}
+
+console.log(aveBalanceByCountry(users));
 //////////////////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////////////////////////
+// A function to group users by country
+function gpUsersByCountry(users) {
+  const countryGroups = {};
+  for (let i = 0; i < users.length; i++) {
+    const country = users[i].country;
+    if (countryGroups[country]) {
+      countryGroups[country].push(users[i]);
+    } else {
+      countryGroups[country] = [users[i]];
+    }
+  }
+  return countryGroups;
+}
 
-//////////////////////////////////////////////////////////////////////////////
+console.log(gpUsersByCountry(users));
+////////////////////////////////////////////////////////////////////////////
 
+// A function to findout under and above 18
+function gpUsersByAge(users) {
+  const ageGroups = { adult: [], child: [] };
+  const currentDate = new Date();
+
+  for (let i = 0; i < users.length; i++) {
+    const userBirthDate = new Date(users[i].birthDate);
+    const userAge = currentDate.getFullYear() - userBirthDate.getFullYear();
+
+    if (userAge >= 18) {
+      ageGroups.adult.push(users[i]);
+    } else {
+      ageGroups.child.push(users[i]);
+    }
+  }
+
+  return ageGroups;
+}
+
+console.log(gpUsersByAge(users));
+////////////////////////////////////////////////////////////////////////////
+
+// A function to get above 18
+function usersAbove18(users) {
+  const usersAbove18 = [];
+  const currentDate = new Date();
+
+  for (let i = 0; i < users.length; i++) {
+    const userBirthDate = new Date(users[i].birthDate);
+    const userAge = currentDate.getFullYear() - userBirthDate.getFullYear();
+
+    if (userAge >= 18) {
+      usersAbove18.push(users[i]);
+    }
+  }
+
+  return usersAbove18;
+}
+
+console.log(usersAbove18(users));
+////////////////////////////////////////////////////////////////////////////
+
+// A function to get users that has >600
+function usersAbove600(users) {
+  const usersWithHighBalance = [];
+  let totalBalance = 0;
+  let count = 0;
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].balance > 600) {
+      usersWithHighBalance.push(users[i]);
+      totalBalance += users[i].balance;
+      count++;
+    }
+  }
+
+  return { users: usersWithHighBalance, averageBalance: totalBalance / count };
+}
+
+console.log(usersAbove600(users));
 //////////////////////////////////////////////////////////////////////////////
